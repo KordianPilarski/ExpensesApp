8a642c4adb97c1df3f2f05ef2d6f4d0e6f25d81c
The-KRD <kaerde91@gmail.com>
2021-07-20 12:13:45 +0200

    add form component, data handlers and form submission

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import './App.css';
-import AddExpense from './components/Expenses/AddExpense'
-import Expenses from './components/Expenses/Expenses';
+import "./App.css";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/newExpense/NewExpense";
 
 const App = () => {
   const expenses = [
-    { id: 'i1', title: 'Life Insurance', amount: 45.4, date: new Date(2021, 2, 3)},
-    { id: 'i2',title: 'Car Insurance', amount: 249.9, date: new Date(2021, 11, 12)},
-    { id: 'i3',title: 'Credit Card Payoff', amount: 89.3, date: new Date(2021, 7, 22)},
-  ]
+    {
+      id: "i1",
+      title: "Life Insurance",
+      amount: 45.4,
+      date: new Date(2021, 2, 3),
+    },
+    {
+      id: "i2",
+      title: "Car Insurance",
+      amount: 249.9,
+      date: new Date(2021, 11, 12),
+    },
+    {
+      id: "i3",
+      title: "Credit Card Payoff",
+      amount: 89.3,
+      date: new Date(2021, 7, 22),
+    },
+  ];
   return (
     <div className="App">
       <h1>
-        <AddExpense />
-        <Expenses items={expenses}/>
+        <NewExpense />
+        <Expenses items={expenses} />
       </h1>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Expenses/AddExpense.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const AddExpense = () => {
-   return <button>Add New Expense</button>
-}
-
-export default AddExpense;
